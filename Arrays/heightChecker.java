d066d0e139909eae2f6b42dc499441252fa33233
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2026-07-20 20:57:26 +0530

    DSA

---
+++ Arrays/heightChecker.java
@@ -0,0 +1,33 @@
+// Height Checker
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+class heightChecker{
+    public static int heightchecker(int[] heights){
+        int[] arr = new int[heights.length];
+        for(int i = 0;i < heights.length; i++){
+            arr[i] = heights[i];
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        for(int i = 0;i < heights.length; i++){
+            if(arr[i] != heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size :");
+        int n = scan.nextInt();
+        System.out.println("Enter the Arrays :");
+        int[] heights = new int[n];
+        for(int i = 0;i < heights.length; i++){
+            heights[i] = scan.nextInt();
+        }
+        int result = heightchecker(heights);
+        System.out.println("student doesn't matching is height with respective order"+result);
+    }
+}
