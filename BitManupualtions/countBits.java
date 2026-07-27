798f54a66669860fb246fbff96467e194abbbc15
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2026-07-27 20:42:47 +0530

    DSA

--- BitManupualtions/countBits.java
+++ BitManupualtions/countBits.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 import java.util.Arrays;
 class countBits{
     public static int[] countingbits(int n){
-        int[] nums = new int[n + 1];
-        for(int i = 0;i <= n; i++) nums[i] = Integer.bitCount(i);
+        int[] nums = new int[n];
+        for(int i = 0;i < n; i++) nums[i] = Integer.bitCount(i);
         return nums;
     }
     public static void main(String[] args){
